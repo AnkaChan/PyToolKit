691b414101f446d7648bc89ec265c236a025c3a0
AnkaChen <ankachan@126.com>
2021-09-14 17:13:25 -0600

    visualizeImgsSortedByLabel
    Signed-off-by: AnkaChen <ankachan@126.com>

--- DeepLearning/Vis.py
+++ DeepLearning/Vis.py
@@ -29,6 +29,49 @@ def visualizeImgsAndLabel(imgs, labels, gridW=10, gridH=10, shuffle=True, cmap='
     if outFile is not None:
         fig.savefig(outFile, dpi=outDPI, transparent=True, bbox_inches='tight', pad_inches=0)
 
+def visualizeImgsSortedByLabel(imgs, labels, gridW=10, gridH=10, randomlyPickLabels=True, shuffle=True, cmap='gray', figTitle=None, outFile=None, outDPI=300, closeExistingFigures=False):
+    import random
+    if closeExistingFigures:
+        plt.close("all")
+    fig, axs = plt.subplots(gridH, gridW)
+    fig.set_size_inches(20*gridW/10, 20*gridH/10)
+    padSize = 100
+
+    allLabelSet = set(labels)
+    allLabels = list(allLabelSet)
+
+    labelsTdDataId = {label:[] for iLabel, label in enumerate(allLabels)}
+
+    if randomlyPickLabels:
+        random.shuffle(allLabels)
+
+    labelsToShow = allLabels[:gridH]
+
+    for i, label in enumerate(labels):
+        labelsTdDataId[label].append(i)
+
+    if shuffle:
+        for label in allLabelSet:
+            random.shuffle(labelsTdDataId[label])
+
+    for i, j in itertools.product(range(gridH), range(gridW)):
+        label = labelsToShow[i]
+
+        if j < len(labelsTdDataId[label]):
+            imgId = labelsTdDataId[label][j]
+            img = imgs[imgId, ...]
+            # pProj = testgtset[i * gridW + j, :]
+            axs[i, j].imshow(np.squeeze(img), cmap=cmap)
+            if labels is not None:
+                axs[i, j].set_title( labels[imgId])
+        axs[i, j].axis('off')
+
+    if figTitle is not None:
+        fig.suptitle(figTitle)
+
+    if outFile is not None:
+        fig.savefig(outFile, dpi=outDPI, transparent=True, bbox_inches='tight', pad_inches=0)
+
 def drawErrCurves(x, y, saveFile=None, title='', xlabel='Epoch', ylabel='Error', closeExistingFigures=False):
     if closeExistingFigures:
         plt.close("all")
